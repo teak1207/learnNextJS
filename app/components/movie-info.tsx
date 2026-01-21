899079924b2cc8335f8ec707af42d6243ccfa251
teak1207 <teak1207@gmail.com>
2026-01-21 23:05:05 +0900

    feat : Deployment

--- app/components/movie-info.tsx
+++ app/components/movie-info.tsx
@@ -1,7 +1,7 @@
 import potato from "../styles/movie-info.module.css";
 import { API_URL } from "../(home)/page";
 
-async function getMovie(id: string) {
+export async function getMovie(id: string) {
   const response = await fetch(`${API_URL}/${id}`);
   return response.json();
 }
